7aaa615ecb763db784c935950ab4f85570bd981e
Kirill Khayrislamov <k.khayrislamov@innopolis.ru>
2021-04-29 15:12:42 +0300

    app/main.py: sync with facerec_module update

--- app/main.py
+++ app/main.py
@@ -5,8 +5,7 @@ import time
 from facerec_module import (
     get_logger,
     FaceDetector,
-    FaceEncoder,
-    FaceVerifier,
+    FaceEncoderDlib,
     face_vector_from_base64_string,
     decode_image_from_base64_string
 )
@@ -28,10 +27,10 @@ app = FastAPI()
 
 logger = get_logger()
 face_detector = FaceDetector(face_confidence=0.97)
-face_encoder = FaceEncoder()
-face_verifier = FaceVerifier()
+face_encoder = FaceEncoderDlib()
 
-max_distance_to_verify = 0.6
+max_distance_to_verify = face_encoder.verify_threshold()
+face_encoding_length = face_encoder.encoding_length()
 
 
 def elapsed_seconds(start_tic):
@@ -52,7 +51,8 @@ def face_encoding_func(request_json, response):
         response["face_rect"] = face_rect
 
         face_encoding = face_encoder.encode(image, [face_rect])[0]
-        assert len(face_encoding) == 128, f"Wrong size of face encoding vector {len(face_encoding)}, expected 128"
+        assert len(face_encoding) == face_encoding_length, \
+            f"Wrong size of face encoding vector {len(face_encoding)}, expected {face_encoding_length}"
 
         return face_encoding
     return None
@@ -95,7 +95,7 @@ async def compare_faces_v1(item: VerifyFaceRequest):
 
             db_face = face_vector_from_base64_string(request["db_face_encoding"])
 
-            distance = face_verifier.verify([db_face], face_encoding)[0]
+            distance = face_encoder.verify([db_face], face_encoding)[0]
             verified = distance < max_distance_to_verify
 
             response["verification"] = VerificationResult(
