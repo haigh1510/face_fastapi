a3757fdf37a191eb3505a986987cb2bb10bdfdb6
Kirill Khayrislamov <haigh1510@gmail.com>
2026-01-16 18:16:30 +0300

    app/main.py: fixed endpoints to return response models

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import argparse
 import base64
-import os
+import numpy as np
 import time
+from typing import Tuple
 
 from facerec_module import (
     get_logger,
@@ -17,6 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from .data_types import (
+    FaceRect,
     EncodeFaceRequest, EncodeFaceResponse,
     VerifyFaceRequest,
     VerificationResult, VerifyFaceResponse,
@@ -54,8 +56,8 @@ def elapsed_seconds(start_tic):
     return time.time() - start_tic
 
 
-def face_encoding_func(request_json, response):
-    image = decode_image_from_base64_string(request_json["image"])
+def face_encoding_func(request: EncodeFaceRequest) -> Tuple[FaceRect, np.array]:
+    image = decode_image_from_base64_string(request.image)
 
     facerec_assert(image.shape[2] == 3,
                    f"Unsupported number of image channels: {image.shape[2]}, expected: 3")
@@ -65,15 +67,14 @@ def face_encoding_func(request_json, response):
     facerec_assert(len(face_bboxes) == 1, f"Found {len(face_bboxes)} faces, expected 1 face")
 
     face_rect = face_bboxes[0]
-    response["face_rect"] = face_rect
 
-    face_encoding = face_encoder.encode(image, [face_rect])[0]
+    face_encoding = face_encoder.encode(image, [face_rect], swapRB=False)[0]
     face_encoding = face_encoding.astype(ENCODER_DTYPE)
 
     facerec_assert(len(face_encoding) == face_encoding_length,
                    f"Wrong size of face encoding vector {len(face_encoding)}, expected {face_encoding_length}")
 
-    return face_encoding
+    return (face_rect, face_encoding)
 
 
 @app.post("/api/v1/encode_face", response_model=EncodeFaceResponse)
@@ -81,24 +82,23 @@ def face_encoding_func(request_json, response):
 async def encode_face_v1(item: EncodeFaceRequest):
     tic = time.time()
 
-    response = {"success": False}
     try:
-        request = item.dict()
+        face_data = face_encoding_func(item)
 
-        face_encoding = face_encoding_func(request, response)
-
-        face_base64 = base64.b64encode(face_encoding)
-        response["face_encoding"] = face_base64.decode('utf-8')
-
-        response["success"] = True
+        return EncodeFaceResponse(
+            face_rect=face_data[0],
+            face_encoding=base64.b64encode(face_data[1]).decode('utf-8'),
+            success=True,
+            seconds=elapsed_seconds(tic)
+        )
     except FacerecError as ex:
-        response["message"] = str(ex)
-    except Exception as ex:
         logger.exception(f"Exception on encode_face_v1 occured:", str(ex))
 
-    response["seconds"] = elapsed_seconds(tic)
-
-    return EncodeFaceResponse(**response)
+        return EncodeFaceResponse(
+            success=False,
+            message=str(ex),
+            seconds=elapsed_seconds(tic)
+        )
 
 
 @app.post("/api/v1/compare_faces", response_model=VerifyFaceResponse)
@@ -106,16 +106,10 @@ async def encode_face_v1(item: EncodeFaceRequest):
 async def compare_faces_v1(item: VerifyFaceRequest):
     tic = time.time()
 
-    response = {"success": False}
     try:
-        request = item.dict()
+        face_encoding = face_encoding_func(item)[1]
 
-        face_encoding = face_encoding_func(request, response)
-
-        face_base64 = base64.b64encode(face_encoding)
-        response["face_encoding"] = face_base64.decode('utf-8')
-
-        db_face = face_vector_from_base64_string(request["db_face_encoding"], dtype=ENCODER_DTYPE)
+        db_face = face_vector_from_base64_string(item.db_face_encoding, dtype=ENCODER_DTYPE)
 
         facerec_assert(len(db_face) == len(face_encoding),
                        f"Incorrect size of db_face: {len(db_face)}, expected: {len(face_encoding)}")
@@ -123,35 +117,38 @@ async def compare_faces_v1(item: VerifyFaceRequest):
         distance = face_encoder.verify([db_face], face_encoding)[0]
         verified = distance < max_distance_to_verify
 
-        response["verification"] = VerificationResult(
-            distance=distance,
-            max_distance_to_verify=max_distance_to_verify,
-            verified=verified).dict()
-
-        response["success"] = True
+        return VerifyFaceResponse(
+            success=True,
+            verification=VerificationResult(
+                distance=distance,
+                max_distance_to_verify=max_distance_to_verify,
+                verified=verified
+            ),
+            seconds=elapsed_seconds(tic)
+        )
     except FacerecError as ex:
-        response["message"] = str(ex)
-    except Exception as ex:
         logger.exception(f"Exception on compare_faces_v1 occured:", str(ex))
 
-    response["seconds"] = elapsed_seconds(tic)
-
-    return VerifyFaceResponse(**response)
+        return VerifyFaceResponse(
+            success=False,
+            seconds=elapsed_seconds(tic)
+        )
 
 
 @app.post("/api/v1/token", response_model=TokenResponse)
 async def get_token_v1(item: TokenData):
-    response = {"success": False}
     try:
-        request = item.dict()
-
-        user_id = request["user_id"]
+        user_id = item.user_id
         # TODO validate user_id
 
-        response["token"] = get_access_token(user_id)
-
-        response["success"] = True
+        return TokenResponse(
+            token=get_access_token(user_id),
+            success=True
+        )
     except Exception as ex:
         logger.exception(f"Exception on token occured:", str(ex))
 
-    return TokenResponse(**response)
+        return TokenResponse(
+            success=False,
+            message=str(ex)
+        )
